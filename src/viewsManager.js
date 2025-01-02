226686315323a1c26ca34bd485b66e130cd35e5a
yuta maeda <yuta.maeda@mangatari.co.jp>
2025-01-03 02:19:14 +0900

    Update viewsManager.js

--- src/viewsManager.js
+++ src/viewsManager.js
@@ -1,9 +1,11 @@
 import { database, ref, set, onValue } from './firebase.js';
 
+// ビューを保存
 export const saveView = (viewName, tasks) => {
     return set(ref(database, `views/${viewName}`), tasks);
 };
 
+// ビューを取得してリスト表示
 export const loadViews = (callback) => {
     onValue(ref(database, 'views'), (snapshot) => {
         const views = snapshot.val();
