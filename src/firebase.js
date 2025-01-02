556754f07b01330734fa7e0f82387abda95d4070
yuta maeda <yuta.maeda@mangatari.co.jp>
2025-01-03 02:50:06 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getDatabase, ref, set, push, onValue, update, remove } from "https://ww
 
 // Firebaseの初期化
 const firebaseConfig = {
-    apiKey: "AIzaSy...",
-    authDomain: "focuscraft-...",
-    databaseURL: "https://focuscraft-...",
-    projectId: "focuscraft-...",
-    storageBucket: "focuscraft-...",
-    messagingSenderId: "3939766...",
-    appId: "1:3939766...de4",
-    measurementId: "G-4YRYK..."
+    apiKey: "AIzaSyAgW5Aqqnx8nN6-7hAOFhQO8K2-UTRYDd8",
+    authDomain: "focuscraft-9693e.firebaseapp.com",
+    databaseURL: "https://focuscraft-9693e-default-rtdb.firebaseio.com",
+    projectId: "focuscraft-9693e",
+    storageBucket: "focuscraft-9693e.firebasestorage.app",
+    messagingSenderId: "393976628205",
+    appId: "1:393976628205:web:18db41e35ed2a2bc381de4",
+    measurementId: "G-4YRYKM6D11"
 };
 
 export const app = initializeApp(firebaseConfig);
